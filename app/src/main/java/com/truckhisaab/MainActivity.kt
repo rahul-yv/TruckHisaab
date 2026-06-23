955549581d94bf6a4e1bf10b7fd388c33cfedfb3
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-24 02:24:07 +0530

    Add MainActivity with edge-to-edge

---
+++ app/src/main/java/com/truckhisaab/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.truckhisaab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.truckhisaab.ui.navigation.TruckHisaabNavGraph
+import com.truckhisaab.ui.theme.TruckHisaabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TruckHisaabTheme {
+                TruckHisaabNavGraph()
+            }
+        }
+    }
+}
