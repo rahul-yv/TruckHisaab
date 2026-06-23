98bf9dc26816985ed67402fbe68171c2a0291ae4
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-24 02:15:50 +0530

    Add brand color palette

---
+++ app/src/main/java/com/truckhisaab/ui/theme/Color.kt
@@ -0,0 +1,32 @@
+package com.truckhisaab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TruckRed = Color(0xFFCD2D28)
+val TruckRedDark = Color(0xFFB71C1C)
+val TruckRedLight = Color(0xFFEF5350)
+val TruckRedBg = Color(0xFFFFEBEE)
+
+val SuccessGreen = Color(0xFF4CAF50)
+val SuccessGreenDark = Color(0xFF2E7D32)
+val SuccessGreenBg = Color(0xFFE8F5E9)
+
+val WarningOrange = Color(0xFFFF9800)
+val WarningOrangeBg = Color(0xFFFFF3E0)
+
+val DangerRed = Color(0xFFF44336)
+val DangerRedBg = Color(0xFFFFEBEE)
+
+val InfoBlue = Color(0xFF2196F3)
+val InfoBlueBg = Color(0xFFE3F2FD)
+
+val WhatsAppGreen = Color(0xFF25D366)
+
+val Background = Color(0xFFF5F5F5)
+val Surface = Color(0xFFFFFFFF)
+val CardBg = Color(0xFFFFFFFF)
+
+val TextPrimary = Color(0xFF333333)
+val TextSecondary = Color(0xFF666666)
+val TextHint = Color(0xFF999999)
+val Divider = Color(0xFFE0E0E0)
